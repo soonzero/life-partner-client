a23e5bdfeb70493ae22992e00d9dbee748c7cfbc
soonzero <sykeum0912@naver.com>
2022-07-15 20:15:10 +0900

    style: 요청 업무 그라데이션 보완, 카드 넓이 변경

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import { addCommasToNumber } from 'functions/common';
 
 const Card = ({ item, detail }: MapInCard) => {
 	return (
-		<article className="group vertical w-60 h-72 rounded-xl border-1 cursor-pointer hover:opacity-90 hover:shadow-main transition overflow-hidden bg-white">
+		<article className="group vertical w-full h-72 rounded-xl border-1 cursor-pointer hover:opacity-90 transition overflow-hidden bg-white">
 			<map className="block w-full h-1/2 z-[2] shrink-0">
 				<NaverMap item={item} detail={detail} />
 			</map>
@@ -12,7 +12,7 @@ const Card = ({ item, detail }: MapInCard) => {
 				<h4 className="text-lg font-bold truncate shrink-0 group-hover:animate-marquee group-hover:w-max">
 					{item.location.split(',')[0]}
 				</h4>
-				<p className="relative text-sm grow overflow-hidden after:content-[''] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-b after:from-transparent after:to-white">
+				<p className="relative text-sm grow overflow-hidden after:content-[''] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-b after:from-transparent after:via-transparent after:to-white">
 					<span className="inline-block break-normal">{item.contents}</span>
 				</p>
 				<p className="flex justify-between">
