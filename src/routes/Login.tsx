8ef1bb2dfdefe897c1b446d8200de107bf8cd1cf
soonzero <sykeum0912@naver.com>
2022-07-16 01:25:17 +0900

    feat: 로그인 페이지에 회원가입 이동 링크와 문구 추가

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,6 +1,7 @@
 import Layout from 'components/Layout';
 import useInputs from 'hooks/useInputs';
 import { FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 const initialState = {
 	nickname: '',
@@ -25,20 +26,22 @@ const Login = () => {
 					className="vertical rounded-lg border-1 border-gray-200 p-8"
 				>
 					<fieldset className="vertical mb-6">
-						<label htmlFor="nickname" className="auth-label">
-							닉네임
-						</label>
-						<input
-							id="nickname"
-							type="text"
-							maxLength={8}
-							minLength={3}
-							placeholder="닉네임을 입력해주세요."
-							className="auth-input"
-							required
-							value={nickname}
-							onChange={onChange}
-						/>
+						<div className="vertical mb-2">
+							<label htmlFor="nickname" className="auth-label">
+								닉네임
+							</label>
+							<input
+								id="nickname"
+								type="text"
+								maxLength={8}
+								minLength={3}
+								placeholder="닉네임을 입력해주세요."
+								className="auth-input"
+								required
+								value={nickname}
+								onChange={onChange}
+							/>
+						</div>
 						<label htmlFor="password" className="auth-label">
 							비밀번호
 						</label>
@@ -57,6 +60,21 @@ const Login = () => {
 					<button type="submit" className="btn-primary">
 						로그인
 					</button>
+					<div className="flex mt-3 center">
+						{/* 아이디 / 비밀번호 찾기 api 추후에 연동하기 */}
+						{/* <Link to="/" className="text-slate-400 text-xs mr-3">
+							아이디 찾기
+						</Link>
+						<Link to="/" className="text-slate-400 text-xs mr-12">
+							비밀번호 찾기
+						</Link> */}
+						<span className="text-slate-500 text-xs">
+							아직 아이디가 없으신가요?{' '}
+							<Link to="/signup" className="font-semibold">
+								회원가입
+							</Link>
+						</span>
+					</div>
 				</form>
 			</section>
 		</Layout>
