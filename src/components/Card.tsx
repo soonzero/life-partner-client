c6bf9443305e575c8989375ab1c4fa5ec6a9b37a
soonzero <sykeum0912@naver.com>
2022-07-15 13:54:01 +0900

    style: 카드 형식 게시글의 주소 부분 marquee 애니메이션 추가

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,17 +5,19 @@ import { addCommasToNumber } from 'functions/common';
 const Card = ({ item }: MapInCard) => {
 	return (
 		<article className="group vertical w-60 h-72 rounded-xl border-1 cursor-pointer hover:opacity-90 hover:shadow-main transition overflow-hidden bg-white">
-			<map className="block w-full h-1/2 z-[2]">
+			<map className="block w-full h-1/2 z-[2] shrink-0">
 				<NaverMap item={item} />
 			</map>
-			<figcaption className="grow vertical m-3 overflow-hidden">
-				<h4 className="text-lg font-bold truncate group-hover:animate-marquee group-hover:w-max">
+			<figcaption className="vertical grow m-3 overflow-hidden">
+				<h4 className="text-lg font-bold truncate shrink-0 group-hover:animate-marquee group-hover:w-max">
 					{item.location.split(',')[0]}
 				</h4>
-				<p className="text-sm grow">{item.contents}</p>
+				<p className="relative text-sm grow overflow-hidden after:content-[''] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-b after:from-transparent after:to-white">
+					<span className="inline-block break-normal">{item.contents}</span>
+				</p>
 				<p className="flex justify-between">
 					<span>{item.period}분 소요</span>
-					<span>{addCommasToNumber(item.price)}원</span>
+					<span className="font-medium">{addCommasToNumber(item.price)}원</span>
 				</p>
 			</figcaption>
 		</article>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,19 @@ module.exports = {
 			boxShadowColor: {
 				main: 'rgba(0, 0, 0, 0.12)',
 			},
+			animation: {
+				marquee: 'marquee 5s linear infinite',
+			},
+			keyframes: {
+				marquee: {
+					'0%': {
+						transform: 'translateX(100%)',
+					},
+					'100%': {
+						transform: 'translateX(-100%)',
+					},
+				},
+			},
 		},
 	},
 	plugins: [],
