3b38295318d7147ffcbe0c6150efd456e4875c97
soonzero <sykeum0912@naver.com>
2022-07-26 02:56:44 +0900

    style: 반응형 breakpoint 설정 및 스타일 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		screens: {
+			xs: '320px',
+			sm: '544px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1440px',
+			'2xl': '1920px',
+			'4xl': '2560px',
+		},
 		extend: {
 			colors: {
 				main: '#69C4E1',
