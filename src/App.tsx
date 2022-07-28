b99a52808763365341134078af8ecf4802cbdf3b
soonzero <sykeum0912@naver.com>
2022-07-28 22:40:32 +0900

    install: react-query 설치 및 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRouter from './routes/AppRouter';
 
 function App() {
-	return <AppRouter />;
+	const queryClient = new QueryClient();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<AppRouter />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
