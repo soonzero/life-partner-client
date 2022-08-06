0ffe557605761fcb882a9dd2f0bca33b0b84d08d
soonzero <sykeum0912@naver.com>
2022-08-06 12:14:56 +0900

    style: footer 최대 너비 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,40 +3,42 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
 	return (
 		<footer>
-			<ul className="vertical space-y-6 md:horizontal md:items-center md:space-x-6 md:space-y-0">
-				<li className="dark:text-white font-bold text-lg">
-					<Link to="/" className="transition hover:no-underline text-main">
-						라이프파트너
-					</Link>
-				</li>
-				<li className="dark:text-white">
-					<Link
-						to="/"
-						className="transition hover:no-underline hover:font-semibold"
-					>
-						개인정보 처리방침
-					</Link>
-				</li>
-				<li className="dark:text-white">
-					<Link
-						to="/"
-						className="transition hover:no-underline hover:font-semibold"
-					>
-						서비스 이용약관
-					</Link>
-				</li>
-				<li className="dark:text-white">
-					<Link
-						to="/"
-						className="transition hover:no-underline hover:font-semibold"
-					>
-						위치기반서비스 이용약관
-					</Link>
-				</li>
-			</ul>
-			<span className="text-xs text-gray-400 mt-12">
-				Copyright© 라이프파트너 All rights reserved.
-			</span>
+			<div className="py-10 mx-auto max-w-screen-2xl w-full vertical justify-between">
+				<ul className="vertical space-y-6 md:horizontal md:items-center md:space-x-6 md:space-y-0">
+					<li className="dark:text-white font-bold text-lg">
+						<Link to="/" className="transition hover:no-underline text-main">
+							라이프파트너
+						</Link>
+					</li>
+					<li className="dark:text-white">
+						<Link
+							to="/"
+							className="transition hover:no-underline hover:font-semibold"
+						>
+							개인정보 처리방침
+						</Link>
+					</li>
+					<li className="dark:text-white">
+						<Link
+							to="/"
+							className="transition hover:no-underline hover:font-semibold"
+						>
+							서비스 이용약관
+						</Link>
+					</li>
+					<li className="dark:text-white">
+						<Link
+							to="/"
+							className="transition hover:no-underline hover:font-semibold"
+						>
+							위치기반서비스 이용약관
+						</Link>
+					</li>
+				</ul>
+				<span className="text-xs text-gray-400 mt-24">
+					Copyright© 라이프파트너 All rights reserved.
+				</span>
+			</div>
 		</footer>
 	);
 };
